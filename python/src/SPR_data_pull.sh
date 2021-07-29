7f00e69548d3b8a80c08f5103e79e111f60049f3
Kaname <kaname.g@gmail.com>
2021-07-29 23:17:18 +0900

    データ展開時に上書きする用に変更

--- python/src/SPR_data_pull.sh
+++ python/src/SPR_data_pull.sh
@@ -4,12 +4,12 @@ cd ./input/santander-product-recommendation
 kaggle competitions download -c santander-product-recommendation
 
 
-unzip santander-product-recommendation.zip
+unzip -o santander-product-recommendation.zip
 rm santander-product-recommendation.zip
 
-unzip sample_submission.csv.zip
-unzip test_ver2.csv.zip
-unzip train_ver2.csv.zip
+unzip -o sample_submission.csv.zip
+unzip -o test_ver2.csv.zip
+unzip -o train_ver2.csv.zip
 
 rm sample_submission.csv.zip
 rm test_ver2.csv.zip
